7c70e0c8562115a10863fc9e40e982ad0486750d
tcassier <donatellosol@protonmail.com>
2023-02-23 17:37:34 +0100

    on va fumer ?

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <div className="bg-[red]">otoo</div>
+      <div className="w-[100vw] h-[100vh]">
+        <div className="text-white absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] relative">
+          <div className="text-5xl text-center">
+            Vous allez bientôt vivre une expérience inédite.
+          </div>
+          <div className="absolute">
+            <img src="../public/scream.png" alt="scream" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
