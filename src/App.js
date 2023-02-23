e0a328f580b0cb8a01b09e1b33b224cd1dbdb13a
tcassier <donatellosol@protonmail.com>
2023-02-23 18:33:10 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -2,15 +2,34 @@ import './App.css';
 import React from 'react';
 
 function App() {
+  const isFirstPage = false;
+
   return (
     <div className="App">
-      <div className="w-[100vw] h-[100vh]">
-        <div className="text-white absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] relative">
-          <div className="text-5xl text-center">
+      <div
+        className={`w-[100vw] h-[100vh] ${
+          isFirstPage ? 'bg-black-background bg-bottom bg-cover' : ''
+        }`}
+      >
+        <div className="text-white absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
+          <div className="text-3xl font-bold text-center">
             Vous allez bientôt vivre une expérience inédite.
           </div>
-          <div className="absolute">
-            <img src="../public/scream.png" alt="scream" />
+
+          <div className="relative">
+            <div className="text-4xl absolute bottom-0 mb-[200px] text-center w-full font-bold">
+              <span className="text-[red]">CRIEZ</span> POUR LANCER LE FILM.
+            </div>
+            <div className="absolute top-0 w-full mt-[80px]">
+              <img
+                className="mx-auto  w-[390px]"
+                src="/scream.png"
+                alt="scream"
+              />
+              <div className="text-center font-bold text-xl">
+                PARTAGEZ VOS RÉACTIONS #SCREAMVI
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      backgroundImage: {
+        'black-background': "url('/public/black-background.png')",
+      },
       colors: {},
       screens: {},
     },
